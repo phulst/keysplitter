0c3ba551cb8e95479b38b93df2bb3016ac30ecc2
Peter Hulst <peter.hulst@springshot.com>
2018-05-17 09:15:39 -0700

    added extension

--- splitter.js
+++ splitter.js
@@ -174,7 +174,7 @@ const key = '3a1076bf45ab87712ad64ccb3b10217737f7faacbf2872e88fdd9a537d8fe266';
 keys = createCodes(key);
 
 for (let i=0; i<3; i++) {
-    generateQrCode(keys[i].toString('base64'), `split_${i+1}`);
+    generateQrCode(keys[i].toString('base64'), `split_${i+1}.png`);
 }
 
 
