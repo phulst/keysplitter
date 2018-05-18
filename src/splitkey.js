0ef7566562455be41f7590a6c964f8d4a57b311f
Peter Hulst <peter.hulst@springshot.com>
2018-05-18 00:44:21 -0700

    command line tool

--- src/splitkey.js
+++ src/splitkey.js
@@ -7,3 +7,11 @@ program
     .option('-s, --split <keyfile>', 'Split a key into 3 secure, redundant parts')
     .option('-r, --restore <splitkey-file> <splitkey-file>', 'Restore a key from 2 split keys')
     .parse(process.argv);
+
+
+
+splitkey create <filename> -qr
+splitkey restore <key1> <key2>
+
+-qr crete QR code
+
